f4aa509d1bef4dd47f4825fe49cb4c831b0e3c6c
agustinbie <agustinbielewicz@hotmail.com>
2023-07-17 23:56:04 -0300

    concecting to the server deploying in render.com

--- smartbrain-app/src/components/Register/Register.js
+++ smartbrain-app/src/components/Register/Register.js
@@ -29,7 +29,7 @@ class Register extends React.Component {
 
 	onSubmitSignIn = () => {
 		
-		fetch("http://localhost:3001/register", {
+		fetch("https://smartbrain-api-eynl.onrender.com/register", {
 			method: "post",
 			headers: {"Content-Type": "application/json"},
 			body: JSON.stringify({
@@ -38,7 +38,7 @@ class Register extends React.Component {
 				name: this.state.name
 			})
 		}).then(response => response.json()).then(user => {    
-			if (user) {		
+			if (user.id) {		
 				this.props.loadUser(user);		
 				this.props.onRouteChange("home");
 				
